e33d6ac942650e0725f06fd30c6489574b9903c4
philschmid <schmidphilipp1995@gmail.com>
2020-05-28 22:20:15 +0200

    finished testing

--- model/model.py
+++ model/model.py
@@ -8,16 +8,21 @@ import base64
 import json
 import re
 
-class ServerlessModel:
-    def __init__(self,model_path=None, s3_bucket=None, file_prefix=None):
-        self.model, self.tokenizer = self.from_pretrained(model_path, s3_bucket, file_prefix)
 
-    def load_model(self, model_path: str):
+class ServerlessModel:
+    def __init__(self, model_path=None, s3_bucket=None, file_prefix=None):
+        self.model, self.tokenizer = self.from_pretrained(
+            model_path, s3_bucket, file_prefix)
+
+    def from_pretrained(self, model_path: str, s3_bucket: str, file_prefix: str):
         if os.path.isfile(f'{model_path}/pytorch_model.bin'):
             model = AutoModelForQuestionAnswering.from_pretrained(
-                model_path,return_token_type_ids = True)
+                model_path)
             config = AutoConfig.from_pretrained(f'{model_path}/config.json')
-        return model
+        else:
+            model = self.load_model_from_s3(model_path, s3_bucket, file_prefix)
+        tokenizer = self.load_tokenizer(model_path)
+        return model, tokenizer
 
     def load_model_from_s3(self, model_path: str, s3_bucket: str, file_prefix: str):
         if model_path and s3_bucket and file_prefix:
@@ -29,8 +34,8 @@ class ServerlessModel:
                 if member.name.endswith(".bin"):
                     f = tar.extractfile(member)
                     state = torch.load(io.BytesIO(f.read()))
-                    model = AutoModelForSequenceClassification.from_pretrained(
-                        pretrained_model_name_or_path=None, state_dict=state, config=config)
+                    model = AutoModelForQuestionAnswering.from_pretrained(
+                        pretrained_model_name_or_path=None, state_dict=state, config=config, return_token_type_ids=True)
             return model
         else:
             raise KeyError('No S3 Bucket and Key Prefix provided')
@@ -38,19 +43,21 @@ class ServerlessModel:
     def load_tokenizer(self, model_path: str):
         tokenizer = AutoTokenizer.from_pretrained(model_path)
         return tokenizer
-        
-    def encode(self,question,context):
+
+    def encode(self, question, context):
         encoded = self.tokenizer.encode_plus(question, context)
         return encoded["input_ids"], encoded["attention_mask"]
 
-    def decode(self,token):
-        answer_tokens = self.tokenizer.convert_ids_to_tokens(token , skip_special_tokens=True)
+    def decode(self, token):
+        answer_tokens = self.tokenizer.convert_ids_to_tokens(
+            token, skip_special_tokens=True)
         return self.tokenizer.convert_tokens_to_string(answer_tokens)
 
-    def predict(self,question,context):
-        input_ids, attention_mask = self.encode(question,context)
-        start_scores, end_scores = self.model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))
-        ans_tokens = input_ids[torch.argmax(start_scores) : torch.argmax(end_scores)+1]
+    def predict(self, question, context):
+        input_ids, attention_mask = self.encode(question, context)
+        start_scores, end_scores = self.model(torch.tensor(
+            [input_ids]), attention_mask=torch.tensor([attention_mask]))
+        ans_tokens = input_ids[torch.argmax(
+            start_scores): torch.argmax(end_scores)+1]
         answer = self.decode(ans_tokens)
         return answer
-
--- test/test_handler.py
+++ test/test_handler.py
@@ -4,8 +4,9 @@ import json
 
 
 test_events = {
-    "body": '{"text": "Who has the most covid-19 deaths?", "context":"The US has passed the peak on new coronavirus cases,President Donald Trump said and predicted that some states would reopen this month. The US has over 637,000 confirmed Covid-19 cases and over 30,826 deaths, the highest for any country in the world."}'
+    "body": '{"question": "Who has the most covid-19 deaths?", "context":"The US has passed the peak on new coronavirus cases,President Donald Trump said and predicted that some states would reopen this month. The US has over 637,000 confirmed Covid-19 cases and over 30,826 deaths, the highest for any country in the world."}'
 }
+
 
 def test_handler():
     res = predict_answer(test_events, '')
